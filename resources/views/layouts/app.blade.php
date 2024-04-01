a2def9e365105f1b327f1699c72b4ef52c9c8cc5
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-04-01 16:26:26 +0800

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,7 @@
           </div>
 
           <!-- Sidebar component, swap this element with another sidebar if you like -->
-          <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2 z-50">
+          <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2 z-20">
             <div class="flex h-16 shrink-0 items-center">
               <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
             </div>
@@ -166,7 +166,7 @@
     </div>
 
     <!-- Static sidebar for desktop -->
-    <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+    <div class="hidden lg:fixed lg:inset-y-0 lg:z-20 lg:flex lg:w-72 lg:flex-col">
       <!-- Sidebar component, swap this element with another sidebar if you like -->
       <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
         <div class="flex h-16 shrink-0 items-center bg-blue-600 px-4 mt-4 rounded-lg text-white ">
