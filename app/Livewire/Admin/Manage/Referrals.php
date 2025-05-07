c76a243862d657d68cb8d9290829b867faf50dee
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-05-07 13:26:50 +0800

    pp

--- app/Livewire/Admin/Manage/Referrals.php
+++ app/Livewire/Admin/Manage/Referrals.php
@@ -28,7 +28,7 @@ class Referrals extends Component implements HasForms, HasTable
         return $table
             ->query(Referral::query())
             ->columns([
-                TextColumn::make('patient.first_name')
+                TextColumn::make('patient.fullName')
                 ->formatStateUsing(fn (PatientInfo $record) => $record->first_name.' '.$record->last_name),
                 TextColumn::make('hospital_name'),
                 TextColumn::make('doctor_name'),
